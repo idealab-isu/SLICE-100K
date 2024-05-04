a426170b31d2f76afb10150df6cbeecbeef4550f
km3888 <km3888@nyu.edu>
2024-05-03 21:58:15 -0400

    finished sailfish absolute extrusion

--- gcode_preprocessing/extrusion.py
+++ gcode_preprocessing/extrusion.py
@@ -1,12 +1,11 @@
 from preprocess_utils import debug, get_layers, make_json, \
                                 get_data, \
-                                convert_strings_to_table \
-
+                                convert_strings_to_table
 import pdb
 import re
 import copy
 
-def sailfish_relative_extrusion(layer):
+def sailfish_relative_extrusion(layer,init_val=None):
     demarcated_layer = demarcate_extrusion_vals(layer, get_initial=False)
     resets = re.finditer(r'G1 E([0-9\.]+) F([0-9]+)', demarcated_layer)
     resets = [match for match in resets]
@@ -18,32 +17,43 @@ def sailfish_relative_extrusion(layer):
     number_indices = [match.start() for match in numbers]
 
     with_initial = []
+    relative_inks = []
     number_i = 0
     # for each reset_i, find the list of number indices n_i such that 
     # reset_i is the last reset index that comes before it
 
-    for i in range(1,len(reset_indices)):
+    for i in range(len(reset_indices)):
         reset_i = reset_indices[i]
         # find the list of number indices n_i such that reset_i is the last reset index that comes before it
         following_nums = []
         while number_i < len(number_indices) and number_indices[number_i] < reset_i:
             following_nums.append(numbers[number_i])
             number_i += 1
-        with_initial.append((resets[i-1], following_nums))
+        if i==0:
+            if len(following_nums)>0:
+                assert init_val is not None
+                with_initial.append((init_val, following_nums))
+            else:
+                relative_inks.append(demarcated_layer[:reset_i])
+        else:
+            with_initial.append((resets[i-1], following_nums))
     with_initial.append((resets[-1], numbers[number_i:]))
     
-    relative_inks = []
     for i in range(len(with_initial)):
         # these are basically their own "inks"
-        reset, number_matches = with_initial[i]
-        init_val = float(reset.group(1))
-        # take substring of layer from this reset to the next reset
-        # this is what we're going to augment and added to relative_inks
-        if i == len(with_initial)-1:
-            relative_ink = demarcated_layer[reset.start():]
+        if i==0 and len(relative_inks)==0:
+            init_val, number_matches = with_initial[i]
+            relative_ink = demarcated_layer[:with_initial[i+1][0].start()]
         else:
-            next_reset = with_initial[i+1][0]
-            relative_ink = demarcated_layer[reset.start():next_reset.start()]
+            reset, number_matches = with_initial[i]
+            init_val = float(reset.group(1))
+            # take substring of layer from this reset to the next reset
+            # this is what we're going to augment and added to relative_inks
+            if i == len(with_initial)-1:
+                relative_ink = demarcated_layer[reset.start():]
+            else:
+                next_reset = with_initial[i+1][0]
+                relative_ink = demarcated_layer[reset.start():next_reset.start()]
 
         
         numbers = [match.group(0) for match in number_matches]
@@ -74,7 +84,112 @@ def sailfish_relative_extrusion(layer):
 
         relative_inks.append(relative_ink)
     relative_layer = ''.join(relative_inks)
-    return relative_layer
+    next_init_val = float(with_initial[-1][0].group(1))
+    return relative_layer,next_init_val
+
+def sailfish_absolute_extrusion(layer,init_val=None):
+    resets = re.finditer(r'G1 E([0-9\.]+) F([0-9]+)', layer)
+    resets = [match for match in resets]
+    reset_indices = [match.start() for match in resets]
+
+    #get indices of all numbers between <e> tags
+    numbers = re.finditer(r'<e>[0-9]*\.?[0-9]*<e>', layer)
+    numbers = [match for match in numbers]
+    number_indices = [match.start() for match in numbers]
+
+    with_initial = []
+    absolute_inks = []
+    number_i = 0
+    # for each reset_i, find the list of number indices n_i such that 
+    # reset_i is the last reset index that comes before it
+    for i in range(len(reset_indices)):
+        reset_i = reset_indices[i]
+        # find the list of number indices n_i such that reset_i is the last reset index that comes before it
+        following_nums = []
+        while number_i < len(number_indices) and number_indices[number_i] < reset_i:
+            following_nums.append(numbers[number_i])
+            number_i += 1
+        if i==0:
+            if len(following_nums)>0:
+                assert init_val is not None
+                with_initial.append((init_val, following_nums))
+            else:
+                absolute_inks.append(layer[:reset_i])
+        else:
+            with_initial.append((resets[i-1], following_nums))
+    with_initial.append((resets[-1], numbers[number_i:]))
+
+    for i in range(len(with_initial)):
+        init = with_initial[i][0]
+
+        # get the portion of layer from the reset to the next reset
+        # if i == len(with_initial)-1:
+        #     absolute_ink = layer[init.start():]
+        # else:
+        #     next_reset = with_initial[i+1][0]
+        #     absolute_ink = layer[init.start():next_reset.start()]
+        
+        #####
+        if i==0 and len(absolute_inks)==0:
+            init_val, number_matches = with_initial[i]
+            absolute_ink = layer[:with_initial[i+1][0].start()]
+        else:
+            reset, number_matches = with_initial[i]
+            init_val = float(reset.group(1))
+            # take substring of layer from this reset to the next reset
+            # this is what we're going to augment and added to relative_inks
+            if i == len(with_initial)-1:
+                absolute_ink = layer[init.start():]
+            else:
+                next_reset = with_initial[i+1][0]
+                absolute_ink =layer[init.start():next_reset.start()]
+        #####
+
+        numbers = [match.group(0) for match in with_initial[i][1]]
+        if not len(numbers):
+            if "<e>" in absolute_ink:
+                pdb.set_trace()
+            absolute_inks.append(absolute_ink)
+            continue
+        # remove the <e> tags
+        numbers_stripped = [number[3:-3] for number in numbers]
+        # convert the numbers to floats
+        float_numbers = [float(number) for number in numbers_stripped]
+        # compute the absolute extrusion
+        absolutes = [init_val + float_numbers[0]]
+        for i in range(1, len(float_numbers)):
+            new_abs_val = absolutes[-1] + float_numbers[i]
+            if len(str(new_abs_val).split('.')[1])>5:
+                old_abs_val = new_abs_val
+                new_abs_val_str = str(new_abs_val)
+
+                # check if value should be rounded up or down
+                if new_abs_val_str.split('.')[1][5] >= '5':
+                    new_abs_val = str(round(new_abs_val, 5))
+                else:
+                    new_abs_val = str(round(new_abs_val- 0.00001, 5))
+                new_abs_val = float(new_abs_val)
+            absolutes.append(new_abs_val)
+            # replace the marked numbers with the absolute extrusion
+        assert min(absolutes) > 0
+        for i in range(len(numbers)):
+            if not all([x.isdigit() or x == '.' for x in str(absolutes[i])]):
+                print(f"Relative value {relative_values[i]} is not a number")
+                pdb.set_trace()
+            str_abs = str(absolutes[i])
+            
+            absolute_ink = absolute_ink.replace(numbers[i], str(absolutes[i]), 1)
+        
+        if "<e>" in absolute_ink:
+            pdb.set_trace()
+        absolute_inks.append(absolute_ink)
+    absolute_layer = ''.join(absolute_inks)
+    absolute_layer = absolute_layer.replace('E0.', 'E.')
+    if '<e>' in absolute_layer:
+        pdb.set_trace()
+    next_init_val = float(with_initial[-1][0].group(1))
+    return absolute_layer,next_init_val
+
 
 def demarcate_extrusion_vals(gcode, get_initial=True):
     marked_lines = []
@@ -174,6 +289,8 @@ def absolute_extrusion(layer):
     Returns:
         str: The layer with absolute extrusion values.
     """
+    if 'G92 E0' not in layer:
+        layer = sailfish_absolute_extrusion(layer)
     # Recover original layer from relative extrusion
     relative_inks = layer.split('G92 E0')
     absolute_inks = [relative_inks[0]]
@@ -223,17 +340,22 @@ def absolute_extrusion(layer):
         absolute_inks.append(ink)
 
     absolute_layer = 'G92 E0'.join(absolute_inks)
-    return absolute_layer
+    return absolute_layer, None
 
 def test_extrusion(args):
     data = get_data(args.data_path,1)
     layers = get_layers(data)
+    init_val = None
     for layer_a,layer_b in layers[1:]:
-        processed = relative_extrusion(layer_b)
-        unprocessed = absolute_extrusion(processed)
-        convert_strings_to_table(processed[:100000],layer_b[:100000])
-        length=1000
-        convert_strings_to_table(layer_a[:length],unprocessed[:length])
-        if layer_a != unprocessed:
+        processed,_ = sailfish_relative_extrusion(layer_b,init_val)
+        unprocessed,init_val = sailfish_absolute_extrusion(processed,init_val)
+        length=175
+        convert_strings_to_table(layer_b[:length],unprocessed[:length])
+        layer_b[:165] == unprocessed[:165]
+        if layer_b != unprocessed:
             print('Error in relative extrusion')
-            pdb.set_trace()
+            for i in range(len(layer_b)):
+                if layer_b[i] != unprocessed[i]:
+                    print(f'Error at index {i}') 
+                    break
+            pdb.set_trace() #there's one layer where it hits this breakpoint but it's just a small rounding error
