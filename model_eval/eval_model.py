882f92e5f14316b416fa9171be22021ea20bbd49
km3888 <km3888@nyu.edu>
2024-05-14 13:35:35 -0400

    fixed iou_list namespace collision

--- model_eval/eval_model.py
+++ model_eval/eval_model.py
@@ -4,7 +4,7 @@ import argparse
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 
 def iou_list(pred, gt):
     pred_layer_dict = parse_gcode(pred)
@@ -36,30 +36,32 @@ if __name__=="__main__":
     experiment_id = args.model_path.split('/')[-2]
     args.experiment_id = experiment_id
 
+    validation_data = os.listdir("/vast/km3888/paired_gcode/thingiverse_10k_marlin/")[6000:]
+    val_example = validation_data[0]
+    
     gt_layers = []
     pred_layers = []
     for layer_idx in range(1, 5):
-        # gt_layer,pred_layer = one_layer_comparison(args,layer_idx)
-        gt_layer,pred_layer = one_layer_comparison(args,layer_idx)
+        gt_layer,pred_layer = one_layer_comparison(args.model_path,layer_idx)
         gt_layers.append(gt_layer)
         pred_layers.append(pred_layer)
     
     pred_gcode = ";LAYER_CHANGE".join(pred_layers)
     gt_gcode = ";LAYER_CHANGE".join(gt_layers)
     iou_lst = iou_list(pred_gcode, gt_gcode)
-    iou_lst = np.array(iou_list)
-    print(iou_list)
-    print(f"IOU: {np.mean(iou_list)}")
-    print(f"Median IOU: {np.median(iou_list)}")
-    print(f"Max IOU: {np.max(iou_list)}")
-    print(f"Min IOU: {np.min(iou_list)}")
-    print(f"Std IOU: {np.std(iou_list)}")
-    print(f"Variance IOU: {np.var(iou_list)}")
-    print(f"IOU list: {iou_list}")
-    print(f"Number of layers: {len(iou_list)}")
+    iou_lst = np.array(iou_lst)
+    print(iou_lst)
+    print(f"IOU: {np.mean(iou_lst)}")
+    print(f"Median IOU: {np.median(iou_lst)}")
+    print(f"Max IOU: {np.max(iou_lst)}")
+    print(f"Min IOU: {np.min(iou_lst)}")
+    print(f"Std IOU: {np.std(iou_lst)}")
+    print(f"Variance IOU: {np.var(iou_lst)}")
+    print(f"IOU list: {iou_lst}")
+    print(f"Number of layers: {len(iou_lst)}")
 
     plt.figure(figsize=(10, 8))
-    plt.hist(iou_list, bins=20, color='blue', alpha=0.7)
+    plt.hist(iou_lst, bins=20, color='blue', alpha=0.7)
     plt.title('IOU Distribution')
     plt.xlabel('IOU')
     plt.ylabel('Frequency')
